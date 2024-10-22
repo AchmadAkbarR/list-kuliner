439c3e6c8fdbd7092a1880ed71acfd7e835c5116
AchmadAkbarR <111202213959@mhs.dinus.ac.id>
2024-10-22 11:12:05 +0700

    masih banyak salah 4:45

---
+++ lib/home_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage(SafeArea safeArea, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/makanan.dart
@@ -0,0 +1,23 @@
+class Makanan {
+  String nama;
+  String deskripsi;
+  String gambarUtama;
+  String detail;
+  String waktuBuka;
+  String harga;
+  String kalori;
+  List<String> gambarLain;
+  List<Map<String, String>> bahan;
+
+  Makanan(this
+  this.nama,
+  this.deskripsi,
+  this.gambarUtama,
+  this.detail,
+  this.waktuBuka,
+  this.harga,
+  this.kalori,
+  this.gambarLain,
+  this.bahan,
+  );
+}
